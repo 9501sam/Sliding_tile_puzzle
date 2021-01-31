1bd9e45ece8e3e6524733ec6f6578923d2f1967d
turtlegod <9501sam@gmail.com>
2021-01-31 16:18:02 +0800

    complete

--- main.cpp
+++ main.cpp
@@ -1,24 +1,69 @@
 #include <iostream>
 #include <cstdio>
+#include <termio.h>
 using namespace std;
 
 #define SIZE 3
 
-void printTable(int table[][SIZE])
+void printTable(char table[][SIZE])
 {
     system("clear");
     printf("+---+---+---+\n");
     for(int i = 0; i < SIZE; i++) {
-        printf("| %d | %d | %d |\n", table[i][0], table[i][1], table[i][2]);
+        printf("| %c | %c | %c |\n", table[i][0], table[i][1], table[i][2]);
         printf("+---+---+---+\n");
     }
+    printf(":");
+}
+
+void swap(char* a, char* b)
+{
+    char tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
 int main()
 {
-    int table[SIZE][SIZE] = {};
+    char table[SIZE][SIZE] = {'4', '7', '2',
+                                '8', '6', '1',
+                                '3', '5', ' '};
+    int r = 2;
+    int c = 2;
 
-    printTable(table);
+    char input = '\0';
+    while(input != 'q'){
+        switch(input){
+            case 'l':
+                if(c-1 >= 0){
+                    swap(&table[r][c], &table[r][c-1]);
+                    c--;
+                }
+                break;
+            case 'k':
+                if(r+1 < SIZE){
+                    swap(&table[r][c], &table[r+1][c]);
+                    r++;
+                }
+                break;
+            case 'j':
+                if(r-1 >= 0){
+                    swap(&table[r][c], &table[r-1][c]);
+                    r--;
+                }
+                break;
+            case 'h':
+                if(c+1 < SIZE){
+                    swap(&table[r][c], &table[r][c+1]);
+                    c++;
+                }
+                break;
+            default:
+                break;
+        }
+        printTable(table);
+        cin >> input;
+    }
 
     return 0;
 }
